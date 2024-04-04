0b83e199c1f29a791591115aa92b06f05290919c
siriwatknp <siriwatkunaporn@gmail.com>
2024-04-04 23:59:21 +0700

    add testDate

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import admin from "firebase-admin";
+import * as functions from "firebase-functions";
+import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
 
 admin.initializeApp();
 
@@ -10,3 +13,17 @@ export { stayLoginGrabFood } from "./stayLoginGrabFood";
 export { buyLV } from "./buyLV";
 // export { notifyLV } from "./notifyLV"; // seems like login is more confident
 export { ghIssueTracker } from "./ghIssueTracker";
+
+export const testDate = functions
+  .region("asia-southeast1")
+  .pubsub.schedule(
+    "* * * * *"
+  )
+  .timeZone("Asia/Bangkok")
+  .onRun(async (context) => {
+    dayjs.extend(timezone);
+    dayjs.tz.setDefault('Asia/Bangkok');
+
+    console.log(dayjs().toString());
+    console.log(dayjs().format('YYYY-MM-DD HH:mm:ss'));
+  });
